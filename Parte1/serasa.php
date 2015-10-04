78583d4c2ab39c1b018f9a2356129a38b3cdcf5a
renato287 <renatosergiolopez@hotmail.com>
2015-10-04 13:20:31 -0300

    Inicio Parte 2

---
+++ Parte1/serasa.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+require 'clienteDAO.php';
+
+$app = new \Slim\Slim();
+
+
--- serasa.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
-require 'clienteDAO.php';
-
-$app = new \Slim\Slim();
-
-
